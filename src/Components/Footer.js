259f05aaf006486e6c7726f1722ec56b3cdb195f
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2021-09-20 16:46:49 -0500

    added linkedin

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import { GoMarkGithub } from "react-icons/go";
+import { GoMarkGithub } from "react-icons/go"
+import { FaLinkedin} from "react-icons/fa"
+
 
 function Footer() {
     return (
         <div>
-            <SocialIcons className='SocialIcon' href="https://github.com/ZacharyCooremans/shopify-challenge">
+            <SocialIcons href="https://github.com/ZacharyCooremans/shopify-challenge">
                 <GoMarkGithub size='3rem'/>
+            </SocialIcons>
+            <SocialIcons href='https://www.linkedin.com/in/zachary-cooremans-a26065208/'>
+                <FaLinkedin size='3rem'/>
             </SocialIcons>
         </div>
     )
